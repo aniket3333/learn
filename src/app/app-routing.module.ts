5e88272f96da3cd25043318a8c4f4f079648c472
aniket <aniket.hiware3@gmail.com>
2025-01-28 17:49:59 +0530

    1 st commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoviesListComponent } from './movie/movies-list/movies-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'movie-list',pathMatch:'full'},
+{path:'movie-list',component:MoviesListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
